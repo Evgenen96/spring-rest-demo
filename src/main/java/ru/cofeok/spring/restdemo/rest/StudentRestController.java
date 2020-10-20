b8443978d6631ef54df14417ed978b021e786b78
Eugene Nikonov <nikonovse96@gmail.com>
2020-10-20 21:16:34 +0400

    added global exception handling with @ControllerAdvice

--- src/main/java/ru/cofeok/spring/restdemo/rest/StudentRestController.java
+++ src/main/java/ru/cofeok/spring/restdemo/rest/StudentRestController.java
@@ -1,9 +1,8 @@
 package ru.cofeok.spring.restdemo.rest;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import ru.cofeok.spring.restdemo.entity.Student;
 
 import javax.annotation.PostConstruct;
@@ -31,6 +30,25 @@ public class StudentRestController {
 
     @GetMapping("/students/{studentId}")
     public Student getStudent(@PathVariable int studentId) {
+        if ((studentId < 0) || (studentId >= theStudents.size())) {
+            throw new StudentNotFoundException("Student id not found - " + studentId);
+        }
         return theStudents.get(studentId);
+    }
+    // add exception handler
+
+
+
+    // another exception handler to catch any type of exceptions
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(Exception exc) {
+
+        // create a StudentErrorResponse
+        StudentErrorResponse error = new StudentErrorResponse();
+        error.setStatus(HttpStatus.BAD_REQUEST.value());
+        error.setMessage(exc.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 }
